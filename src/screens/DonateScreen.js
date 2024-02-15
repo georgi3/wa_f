5cbd64aa3f99604be816f9f7e65b86bbf3202bfd
Abdulrahman M <53632818+Aoof@users.noreply.github.com>
2024-02-15 16:33:48 -0500

    Fix button in DonateScreen.js

--- src/screens/DonateScreen.js
+++ src/screens/DonateScreen.js
@@ -20,7 +20,7 @@ export default function DonateScreen(){
                         <Row className={"w-100 d-flex"}>
                             <Col  sm={12} lg={6} className={""}>
                                 <div className={"w-100"}>
-                                    <Link className={"w-100 btn btn-lg btn-outline-info m-1 dbox-donation-button donate-btn"} to={"https://donorbox.org/tis-the-season-of-giving-1"}>Donate Funds</Link>
+                                    <Link className={"w-100 btn btn-lg btn-info m-1 dbox-donation-button donate-btn"} to={"https://donorbox.org/tis-the-season-of-giving-1"}>Donate Funds</Link>
                                 </div>
                             </Col>
                             <Col  sm={12} lg={6} className={""}>
