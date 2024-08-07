0fb0be7726ef6e8e32a8d3457fd5f5b5d0f1669a
Abdulrahman Mousa <aoofxd@gmail.com>
2024-08-06 21:41:46 -0400

    [Apoorva Commit] Changes to PostNewsletterScreen

--- src/screens/PostNewsletterScreen/PostNewsletterScreen.js
+++ src/screens/PostNewsletterScreen/PostNewsletterScreen.js
@@ -1,34 +1,45 @@
 
-import React from "react";
+import React,{useEffect,useRef} from "react";
 import {Container} from "react-bootstrap";
 import './PostNewsletterScreen.scss';
 import MyNavbar from "../../components/Navbar";
 import Footer from "../../components/Footer/Footer";
 import ImageLoader from "../../components/ImageLoader";
 import img1 from '../../assets/images/landingPage/landing-page-pic1.jpg';
-import { faFacebook, faInstagram, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function PostNewsletterScreen (){
+    const textRefs = useRef([]);
     React.useEffect(() => {
         window.gtag('event', 'newsletter-signup' , { 'event_category': 'engagement', 'event_label': 'newsletter-signup', 'value': '1' });
+        const animationDurations = [1500, 1500]; 
+
+        textRefs.current.forEach((ref, index) => {
+            setTimeout(() => {
+                ref.classList.add('typing-started');
+                setTimeout(() => {
+                    ref.classList.add('typing-finished')
+                },animationDurations[index]);
+            },index === 0 ? 0 : animationDurations[index - 1]);   
+        });
     }, []);
 
     return(
         <>
             <MyNavbar />
+            <div class="newsletter-wrapper">
             <Container className={"post-newsletter"}>
                 <Container className={"little-box"}>
                     <div className={"newsletter-left-part"}>
                         <ImageLoader className={"newsletter-image"} src={img1} alt={"People lining up for food."} />
                     </div>
                     <div className={"newsletter-right-part"}>
-                        <h3>Thank you for registering to our newsletter.</h3>
-                        <p>Stay tuned for updates on our latest events, fundraisers, and volunteering opportunities.</p>
+                        <h3 class="animation" ref={(el)=>textRefs.current[0]=el}>Thank you for registering to our newsletter!</h3>
+                        <p class="animation" ref={(el)=>textRefs.current[1]=el}>Stay tuned for updates on our latest events, fundraisers, and volunteering opportunities.</p>
                     </div>
                 </Container>
             </Container>
             <Footer />
+            </div>    
         </>
     )
 }
